b9b8873958b67487db0e0216240184444bd665e5
Jodie Drover <jodiedrover@live.com>
2022-10-20 18:41:40 -0400

    Days with 10 or more assignments

--- 3_queries_group-by/2_10_Assignments.sql
+++ 3_queries_group-by/2_10_Assignments.sql
@@ -1,8 +1,11 @@
 -- SELECT assignments.day AS day, count(assignments.id) as total_assignments
 -- FROM assignments
--- GROUP BY assignments.day
+-- GROUP BY day
+-- ORDER BY day
+-- HAVING count(assignments.id) >= 10;
 
 SELECT day, count(*) as total_assignments 
 FROM assignments
 GROUP BY day
+HAVING count(*) >= 10
 ORDER BY day;
