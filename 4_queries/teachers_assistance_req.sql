b0fadf7bdb32ee8049022c57daa60be7a57b7a75
Jodie Drover <jodiedrover@live.com>
2022-10-25 11:46:19 -0400

    Query for total assistance requests for a student

---
+++ 4_queries/teachers_assistance_req.sql
@@ -0,0 +1,11 @@
+-- SELECT count(assistance_request.*) as total_assistances, teachers.name as name
+-- FROM assistance_request
+-- JOIN teachers ON assistance_request.teacher_id = teachers.id
+-- WHERE teachers.name = 'Waylon Boehm'
+-- GROUP BY teachers.name;
+
+SELECT count(assistance_request.*) as total_assistances, teachers.name
+FROM assistance_request
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
