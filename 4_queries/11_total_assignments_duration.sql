c8f608afd71147ce9992346bbec2498f822509d5
Jodie Drover <jodiedrover@live.com>
2022-10-25 18:48:10 -0400

    Query for teachers that assisted during a cohort

--- 4_queries/11_total_assignments_duration.sql
+++ 4_queries/11_total_assignments_duration.sql
@@ -1,4 +1,4 @@
-SELECT day, count(assignments) as number_of_assignments, sum(duration) as duration
+SELECT day, count(*) as number_of_assignments, sum(duration) as duration
 FROM assignments
 GROUP BY day
 ORDER BY day;
